cdef3e0d5256da685ad7e7d9b3e2b618500f41de
Steven Le <stevenleusa79@yahoo.com>
2024-05-03 03:11:10 -0700

    schema changes + exercises CRUD stuff works, need to update with users

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -5,7 +5,7 @@ const asyncHandler = require('express-async-handler')
 // @access    Private
 const getExercises = asyncHandler(async (req, res) => {
   const db = req.app.get('db');
-  const sql = "SELECT * FROM users";
+  const sql = "SELECT * FROM exercises";
   db.query(sql, (err, result) => {
     if (err) return res.json({ error: err });
     return res.json(result);
@@ -16,28 +16,72 @@ const getExercises = asyncHandler(async (req, res) => {
 // @route     POST /api/exercises
 // @access    Private
 const setExercise = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  const { name, muscle_group } = req.body;
+
+  if (!name || !muscle_group) {
     res.status(400);
-    throw new Error('Please add a text field');
+    throw new Error('Please provide both name and muscle_group fields');
   }
 
-  return res.status(200).json({ message: 'Set exercise' });
+  const db = req.app.get('db');
+  const sql = 'INSERT INTO Exercises (name, muscle_group) VALUES (?, ?)';
+  const values = [name, muscle_group];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500);
+      throw new Error('Failed to set exercise');
+    }
+
+    return res.status(200).json({ message: 'Exercise added successfully!' });
+  });
 });
 
 // @desc      Update exercise
 // @route     PUT /api/exercises/:id
 // @access    Private
 const updateExercise = asyncHandler(async (req, res) => {
+  const { name, muscle_group } = req.body;
+  const exerciseId = req.params.id;
 
+  if (!name || !muscle_group) {
+    res.status(400);
+    throw new Error('Please provide both name and muscle_group fields');
+  }
+
+  const db = req.app.get('db');
+  const sql = 'UPDATE Exercises SET name = ?, muscle_group = ? WHERE EID = ?';
+  const values = [name, muscle_group, exerciseId];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500);
+      throw new Error('Failed to update exercise');
+    }
+
+    return res.status(200).json({ message: 'Exercise updated successfully!' });
+  });
 });
 
 // @desc      Delete exercise
 // @route     DELETE /api/exercises/:id
 // @access    Private
 const deleteExercise = asyncHandler(async (req, res) => {
+  const exerciseId = req.params.id;
 
+  const db = req.app.get('db');
+  const sql = 'DELETE FROM Exercises WHERE EID = ?';
+  const values = [exerciseId];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500);
+      throw new Error('Failed to delete exercise');
+    }
+
+    return res.status(200).json({ message: 'Exercise deleted successfully!' });
+  });
 });
-
 
 module.exports = {
   getExercises,
