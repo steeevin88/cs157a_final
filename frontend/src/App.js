98297d8829f428d3a0ab62dd4bc2e5422e49b34a
Ysabella <ysabelladelacruz2022@gmail.com>
2024-05-04 02:29:32 -0700

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <div className="">
-      <header className="">
-        <img src='' className="" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className=""
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header /> 
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
