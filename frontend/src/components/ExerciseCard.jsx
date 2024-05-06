7cadabbb4f1e2a1340600a52ced740db972ba767
Ethpa <eth0410@gmail.com>
2024-05-06 00:09:43 -0700

    wrapped the entire card with the link and changed some styling

--- frontend/src/components/ExerciseCard.jsx
+++ frontend/src/components/ExerciseCard.jsx
@@ -7,15 +7,15 @@ function ExerciseCard({ exercise }) {
   const dispatch = useDispatch()
 
   return (
-    <div className='border-2 border-black rounded-lg exercise'>
-      <Link to={`/exercise/${exercise.EID}`}>
-        <h2>{exercise.name}</h2>
-      </Link>
-      <h4>{exercise.muscle_group}</h4>
-      <button onClick={() => dispatch(deleteExercise(exercise.EID))} className='close'>
-        X
-      </button>
-    </div>
+    <Link to={`/exercise/${exercise.EID}`}>
+      <div className='border-2 border-black rounded-lg exercise'>
+        <p className="mb-2 text-2xl font-bold">{exercise.name}</p>
+        <h4>{exercise.muscle_group}</h4>
+        <button onClick={() => dispatch(deleteExercise(exercise.EID))} className='close'>
+          X
+        </button>
+      </div>
+    </Link>
   )
 }
 
