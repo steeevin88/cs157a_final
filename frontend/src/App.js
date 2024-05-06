cb59efcdbbe08bab37ebde00bd03017d8ab8f33d
Ethpa <eth0410@gmail.com>
2024-05-05 20:32:38 -0700

    added links to individual exercise page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Exercise from './pages/Exercise'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/exercise/:eid' element={<Exercise />} />
           </Routes>
         </div>
       </Router>
--- frontend/src/components/ExerciseCard.jsx
+++ frontend/src/components/ExerciseCard.jsx
@@ -1,13 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { deleteExercise } from '../features/exercises/exerciseSlice'
 
+import { Link } from 'react-router-dom';
+
 function ExerciseCard({ exercise }) {
   const dispatch = useDispatch()
-  console.log(exercise)
 
   return (
     <div className='border-2 border-black rounded-lg exercise'>
-      <h2>{exercise.name}</h2>
+      <Link to={`/exercise/${exercise.EID}`}>
+        <h2>{exercise.name}</h2>
+      </Link>
       <h4>{exercise.muscle_group}</h4>
       <button onClick={() => dispatch(deleteExercise(exercise.EID))} className='close'>
         X
